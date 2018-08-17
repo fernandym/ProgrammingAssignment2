a34e17c4a71698111e21a52019fc57a92ad1a22b
fernandym <40564902+fernandym@users.noreply.github.com>
2018-08-17 19:11:58 +0700

    Basic R Week 3 Assginment
    Function that computes the inverse of the special "matrix" returned by makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,14 @@
 
 ## Write a short comment describing this function
 
+# makeCacheMatrix creates a list containing a function to
+# 1. set the value of the matrix
+# 2. get the value of the matrix
+# 3. set the value of inverse of the matrix
+# 4. get the value of inverse of the matrix
+
 makeCacheMatrix <- function(x = matrix()) {
+     
      inv <- NULL
      set <- function(y) {
           x <<- y
@@ -22,7 +29,6 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-     ## Return a matrix that is the inverse of 'x'
      inv <- x$getinverse()
      if(!is.null(inv)) {
           message("getting cached data")
